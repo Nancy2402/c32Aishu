73cbf4534e3281a5a0c6a8710166ef301b0c25b0
Nancy2402 <60804340+Nancy2402@users.noreply.github.com>
2021-04-17 18:37:23 +0530

    Add files via upload

---
+++ Board.js
@@ -0,0 +1,15 @@
+class Board{
+    constructor(x,y){
+        var option={
+            isStatic:true
+        }
+        this.r=50;
+        this.body = Bodies.circle(x,y,50,option);
+        this.image=loadImage("board.png");
+        World.add(world,this.body);
+    }
+    display(){
+        imageMode(CENTER);
+        image(this.image,this.body.position.x,this.body.position.y,225,225);
+    }
+}
